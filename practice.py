48e925925ae01869bad0b4034bbe701307780230
minhtu-nguyen <minhtu-nguyen@outlook.com>
2025-12-25 23:33:22 +0700

    [Daily] Anagrams

--- practice.py
+++ practice.py
@@ -3,6 +3,8 @@ Array - Hash
 '''
 from typing import List
 
+from matplotlib import collections
+
 def containsDuplicate(nums: List[int]) -> bool:
     hashset = set()
 
@@ -31,3 +33,13 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         if diff in prevMap:
             return [prevMap[diff], i]
         prevMap[n] = i
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    ans = collections.defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        ans[tuple(count)].append(s)
+    return ans.values()
