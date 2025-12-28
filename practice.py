0c6a6d7413d31f6561b13480d7295e7888af203b
minhtu-nguyen <minhtu-nguyen@outlook.com>
2025-12-28 18:46:38 +0700

    [Daily] Product Except Self

--- practice.py
+++ practice.py
@@ -62,3 +62,15 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
             if len(res) == k:
                 return res
             
+def productExceptSelf(nums: List[int]) -> List[int]:
+    res = [1] * len(nums)
+
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+    return res
