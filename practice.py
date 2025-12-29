fe2d379ae64e8d8a6db490b731af5a120b2dbd32
minhtu-nguyen <minhtu-nguyen@outlook.com>
2025-12-29 22:17:55 +0700

    [Daily] Valid Sudoku

--- practice.py
+++ practice.py
@@ -74,3 +74,23 @@ def productExceptSelf(nums: List[int]) -> List[int]:
         res[i] *= postfix
         postfix *= nums[i]
     return res
+
+def isValidSudoku(board: List[List[str]]) -> bool:
+    cols = defaultdict(set)
+    rows = defaultdict(set)
+    squares = defaultdict(set)
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+            if (
+                board[r][c] in rows[r]
+                or board[r][c] in cols[c]
+                or board[r][c] in squares[(r // 3, c // 3)]
+            ):
+                return False
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r][c])
+    return True
