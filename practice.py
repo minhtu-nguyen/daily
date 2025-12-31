f6718f6ea3dd330352638f43243b4074022c02a7
minhtu-nguyen <minhtu-nguyen@outlook.com>
2025-12-31 22:54:09 +0700

    [Daily] Is Palindrome

--- practice.py
+++ practice.py
@@ -106,3 +106,27 @@ def longestConsecutive(nums: List[int]) -> int:
                 length += 1
             longest = max(longest, length)
     return longest
+
+'''
+Two Pointers
+'''
+def alphanum(c: str) -> bool:
+    return (
+        ord("A") <= ord(c) <= ord("Z")
+        or ord("a") <= ord(c) <= ord("z")
+        or ord("0") <= ord(c) <= ord("9")
+    )
+
+def isPalindrome(s: str) -> bool:
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        while left < right and not alphanum(s[left]):
+            left += 1
+        while left < right and not alphanum(s[right]):
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return False
