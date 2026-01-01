09457ba3b7ddb860350ce84cbe55f16e98fa7699
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-01 23:26:03 +0700

    [Daily] Two Sum II

--- practice.py
+++ practice.py
@@ -130,3 +130,16 @@ def isPalindrome(s: str) -> bool:
         left += 1
         right -= 1
     return False
+
+def twoSumII(numbers: List[int], target: int) -> List[int]:
+    left, right = 0, len(numbers) - 1
+
+    while left < right:
+        curSum = numbers[left] + numbers[right]
+
+        if curSum > target:
+            right -= 1
+        elif curSum < target:
+            left += 1
+        else:
+            return [left + 1, right + 1]
