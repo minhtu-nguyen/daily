8b94140c96a5fe8853f383b4ecfda020464cb60a
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-09 22:01:05 +0700

    [Daily] Min Stack

--- practice.py
+++ practice.py
@@ -206,3 +206,22 @@ def isValidParentheses(s: str) -> bool:
         stack.pop()
     return not stack
 
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        return self.minStack[-1]
