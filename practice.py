f436bcd8cc527aeb923a0e62d94fa78fbb7eed39
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-11 10:19:16 +0700

    [Daily] Eval RPN

--- practice.py
+++ practice.py
@@ -225,3 +225,20 @@ class MinStack:
     
     def getMin(self) -> int:
         return self.minStack[-1]
+    
+def evalPRN(tokens: List[str]) -> int:
+    stack = []
+    for c in tokens:
+        if c == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif c == "-":
+            a, b = stack.pop(), stack.pop()
+            stack.append(b - a)
+        elif c == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif c == "/":
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(b / a))
+        else:
+            stack.append(int(c))
+    return stack[0]
