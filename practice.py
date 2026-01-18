3d9deb880c0b7284821e4264c3d609ebcda72d93
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-18 22:19:35 +0700

    [Daily] Car Fleet

--- practice.py
+++ practice.py
@@ -278,3 +278,14 @@ def dailyTemperatures(temps: List[int]) -> List[int]:
     return res
 
 
+def carFleet(target: int, position: List[int], speed: List[int]) -> int:
+    pair = [(pos, spd) for pos, spd in zip(position, speed)]
+    pair.sort(reverse=True)
+    stack = []
+
+    for pos, spd in pair:
+        stack.append((target - pos) / spd)
+        if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            stack.pop()
+    return len(stack)
+
