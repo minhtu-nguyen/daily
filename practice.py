d95e537588f901188475b9e8dddbf76693aa1140
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-19 20:31:01 +0700

    [Daily] Max Rectangle Area

--- practice.py
+++ practice.py
@@ -289,3 +289,18 @@ def carFleet(target: int, position: List[int], speed: List[int]) -> int:
             stack.pop()
     return len(stack)
 
+def largestRectangleArea(heights: List[int]) -> int:
+    maxArea = 0
+    stack = []
+
+    for i, h in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > h:
+            index, height = stack.pop()
+            maxArea = max(maxArea, height * (i - index))
+            start = index
+        stack.append((start, h))
+
+    for i, h in stack:
+        maxArea = max(maxArea, h * (len(heights) - i))
+    return maxArea
