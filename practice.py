472eedab7a1c000cd0d8d4cf41d0e44859fece69
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-21 22:24:49 +0700

    [Daily] Longest Substring

--- practice.py
+++ practice.py
@@ -319,3 +319,15 @@ def maxProfit(prices: List[int]) -> int:
         res = max(res, price - lowest)
     return res
 
+def lengthOfLongestSubstring(s: str) -> int:
+    charSet = set()
+    left = 0
+    res = 0
+
+    for right in range(len(s)):
+        while s[right] in charSet:
+            charSet.remove(s[left])
+            left += 1
+        charSet.add(s[right])
+        res = max(res, right - left + 1)
+    return res
