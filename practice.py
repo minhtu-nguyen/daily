9cc26d447becd8588902e5f21054623d46b2be42
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-22 22:23:08 +0700

    [Daily] Character Replacement

--- practice.py
+++ practice.py
@@ -331,3 +331,19 @@ def lengthOfLongestSubstring(s: str) -> int:
         charSet.add(s[right])
         res = max(res, right - left + 1)
     return res
+
+def characterReplacement(s: str, k: int) -> int:
+    count = {}
+    left = 0
+    maxCount = 0
+
+    for right in range(len(s)):
+        count[s[right]] = 1 + count.get(s[right], 0)
+        maxCount = max(maxCount, count[s[right]])
+
+        if (right - left + 1) - maxCount > k:
+            count[s[left]] -= 1
+            left += 1
+    
+    return right - left + 1
+
