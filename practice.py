2587e2d455212ed5593f989b1158af5897a18d18
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-01-29 22:59:19 +0700

    [Daily] Min Window

--- practice.py
+++ practice.py
@@ -400,3 +400,34 @@ def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
             left += 1
         right += 1
     return output
+
+def minWindow(s: str, t: str) -> str:
+    if t == "":
+        return ""
+    
+    countT, window = {}, {}
+    for c in t:
+        countT[c] = 1 + countT.get(c, 0)
+
+    have, need = 0, len(countT)
+    res, resLen = [-1, -1], float("infinity")
+
+    left = 0
+    for right in range(len(s)):
+        c = s[right]
+        window[c] = 1 + window.get(c, 0)
+
+        if c in countT and window[c] == countT[c]:
+            have += 1
+
+        while have == need:
+            if (right - left + 1) < resLen:
+                res = [left, right]
+                resLen = right - left + 1
+            
+            window[s[left]] -= 1
+            if s[left] in countT and window[s[left]] < countT[s[left]]:
+                have -= 1
+            left += 1
+    left , right = res
+    return s[left : right + 1] if resLen != float("infinity") else ""
