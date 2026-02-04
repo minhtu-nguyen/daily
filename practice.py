4ae903819c1f3dccc4b46b62e2a899904cfb6af3
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-02-04 22:59:34 +0700

    [Daily] Reorder List

--- practice.py
+++ practice.py
@@ -480,3 +480,24 @@ def hasCycle(head: ListNode) -> bool:
         if slow == fast:
             return True
     return False
+
+def reorderList(head: ListNode) -> None:
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    second = slow.next
+    prev = slow.next = None
+    while second:
+        temp = second.next
+        second.next = prev
+        prev = second
+        second = temp
+
+    first, second = head, prev
+    while second:
+        temp1, temp2 = first.next, second.next
+        first.next = second
+        second.next = temp1
+        first, second = temp1, temp2
