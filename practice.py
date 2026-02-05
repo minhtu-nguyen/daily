1c93eadbfd8857711e8630fdd3a2a73666bde82f
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-02-05 23:11:24 +0700

    [Daily] Remove Nth from End

--- practice.py
+++ practice.py
@@ -501,3 +501,19 @@ def reorderList(head: ListNode) -> None:
         first.next = second
         second.next = temp1
         first, second = temp1, temp2
+
+def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
+    dummy = ListNode(0, head)
+    left = dummy
+    right = head
+
+    while n > 0:
+        right = right.next
+        n-= 1
+
+    while right:
+        left = left.next
+        right = right.next
+    
+    left.next = left.next.next
+    return dummy.next
