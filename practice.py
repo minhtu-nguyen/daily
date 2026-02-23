a201e3c41869b203763ec5aced5148cb2eab49ee
minhtu-nguyen <minhtu-nguyen@outlook.com>
2026-02-23 22:19:04 +0700

    [Daily] Copy Random List

--- practice.py
+++ practice.py
@@ -517,3 +517,25 @@ def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
     
     left.next = left.next.next
     return dummy.next
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+def copyRandomList(head: Node) -> Node:
+    oldToCopy = {None: None}
+
+    cur = head
+    while cur:
+        copy = Node(cur.val)
+        oldToCopy[cur] = copy
+        cur = cur.next
+    cur = head
+    while cur:
+        copy = oldToCopy[cur]
+        copy.next = oldToCopy[cur.next]
+        copy.random = oldToCopy[cur.random]
+        cur = cur.next
+    return oldToCopy[head]
